3e76775338273217d21714d22b0f07fa1db1e258
Duck-Dev687 <duckdev32@gmail.com>
2024-09-05 19:01:50 +0300

    c1

--- thewebsite/tailwind.config.js
+++ thewebsite/tailwind.config.js
@@ -5,26 +5,26 @@ module.exports = {
     extend: {
       colors: {
         papayawhip: {
-          light: '#fef4e4',
-          DEFAULT: '#ffefd5',
-          dark: '#fee5bc',
-        }
+          light: "#fefe4",
+          DEFAULT: "#ffefd5",
+          dark: "#fee5bc",
+        },
       },
       screens: {
-        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
-        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
       },
       keyframes: {
-        'open-menu': {
-          '0%': { transform: 'scaleY(0)' },
-          '80%': { transform: 'scaleY(1.2)' },
-          '100%': { transform: 'scaleY(1)' },
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
         },
       },
       animation: {
-        'open-menu': 'open-menu 0.5s ease-in-out forwards',
-      }
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
-}
+};
